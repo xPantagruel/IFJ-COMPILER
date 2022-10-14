5e1ece0a453c3745f0eefc6b8e9f026a4446fed4
Dominik Petrik <dominik.petrik@tiscali.cz>
2022-10-14 15:22:22 +0200

    add base structures and functions for bottom-up

--- parser.c
+++ parser.c
@@ -1,11 +1,45 @@
-#include "scanner.h"
+/**
+ * @file parser.c
+ *
+ * @author Martin Kubička (xkubic45)
+ * @author Matěj Macek (xmacek27)
+ * @author Dominik Petřík (xpetri25)
+ * @author Lukáš Zítko (xzitko00)
+ *
+ * @brief Implementation of syntax analysis of PHP like language.
+ *
+ * @date 2022-10-14
+ */
 
-int main() {
+#include "parser.h"
+
+void initExpression(Expression *exp)
+{
+    exp->arrayLen = 0;
+    exp->tokenArray = NULL;
+}
+
+bool addTokenToExpression(Expression *exp, Token *token)
+{
+    exp->tokenArray = realloc(exp->tokenArray, sizeof((exp->arrayLen + 1) * sizeof(Token *)));
+    if (exp->tokenArray == NULL)
+    {
+        return false;
+    }
+    exp->tokenArray[exp->arrayLen] = token;
+    exp->arrayLen++;
+    return true;
+}
+
+int main()
+{
     Token *token = getToken();
-    while (strcmp(token->val, "EOF")) {
+    while (strcmp(token->val, "EOF"))
+    {
         printf("%s %d\n", token->val, token->t);
         dtorToken(token);
         token = getToken();
     }
-    printf("%s\n", token->val); //print EOF
+    printf("%s\n", token->val);
+    // print EOF
 }
--- parser.h
+++ parser.h
@@ -1,1 +1,27 @@
+/**
+ * @file parser.h
+ *
+ * @author Martin Kubička (xkubic45)
+ * @author Matěj Macek (xmacek27)
+ * @author Dominik Petřík (xpetri25)
+ * @author Lukáš Zítko (xzitko00)
+ *
+ * @brief Declatarions of enums, functions and variables used in parser.c.
+ *
+ * @date 2022-10-12
+ */
 
+#include "scanner.h"
+#include <stdbool.h>
+
+typedef struct Expression
+{
+    Token **tokenArray;
+    int arrayLen;
+} Expression;
+
+bool topDown(Expression *exp);
+
+void initExpression(Expression *exp);
+
+bool addTokenToExpression(Expression *exp, Token *token);
