eaefd4e15603bb461fd2b184894bd0885b9f9086
Dominik Petrik <dominik.petrik@tiscali.cz>
2022-12-06 20:20:27 +0100

    add comments

--- bottomUp.h
+++ bottomUp.h
@@ -38,46 +38,106 @@ typedef struct term
     Token *originalToken;
 } Term;
 
+/// @brief Initialize variable of type Term
+/// @param value of the term
+/// @param type of the term
+/// @param originalToken token from which term is created
+/// @return pointer to new term
 Term *initTerm(char *value, int type, Token *originalToken);
 
+/// @brief Ovewrite term value with new value
+/// @param t term to overwrite value of
+/// @param value new value
 void changeTermValue(Term *t, char *value);
 
+/// @brief Reduce terms to new term
+/// @param expression term to reduce to
+/// @param stack stack of terms
+/// @return number of terms reduced
 int loadTermsToExpression(Term *expression, Stack *stack);
 
+/// @brief Set type of new term
+/// @param t1
+/// @param operator,Term*t2
+/// @return type of new term
 int convertTypesInReduce(Term *t1, Term *operator, Term * t2);
 
+/// @brief Rule for +,-,*
+/// @param t1 first term
+/// @param t2 second term
+/// @return type of new term
 int convertPlusMinusMul(Term *t1, Term *t2);
 
+/// @brief Rule for /
+/// @param t1 first term
+/// @param t2 second term
+/// @return type of new term
 int convertDiv(Term *t1, Term *t2);
 
+/// @brief Rule for .
+/// @param t1 first term
+/// @param t2 second term
+/// @return type of new term
 int convertDot(Term *t1, Term *t2);
 
+/// @brief Rule for ===,!== and relation operators
+/// @param t1 first term
+/// @param t2 second term
+/// @return type of new term
 int convertCompareEquals(Term *t1, Term *t2);
 
+/// @brief Rule for ===,!==
+/// @param t1 term
+/// @return type of new term
 int convertBrackets(Term *t);
 
+/// @brief convert type of term to type of token
+/// @param type of term
+/// @param originalType type of original token of term
+/// @return token type
 int convertTermToTokenType(int type, int originalType);
 
+/// @brief convert type of token to type of term
+/// @param type of token
+/// @return type of term
 int convertTokenToTermType(int type);
 
+/// @brief reduces terms to expression term
+/// @param stack to reduce terms on
+/// @return true on success, false on error
 bool reduce(Stack *stack);
 
+/// @brief runs terms through rules for reducing
+/// @param terms to reduce
+/// @param termCount number of terms
+/// @return true on success, false on error
 bool ruleDecider(Term *terms[3], int termCount);
 
+/// @brief identifier rule
 bool ruleIdentifier(Term *t);
 
+/// @brief + - rule
 bool rulePlusMinus(Term *t1, Term *operator, Term * t2);
 
+/// @brief . rule
 bool ruleConcat(Term *t1, Term *operator, Term * t2);
 
+/// @brief * / rule
 bool ruleMultDiv(Term *t1, Term *operator, Term * t2);
 
+/// @brief () rule
 bool ruleBracket(Term *leftBracket, Term *t1, Term *rightBracket);
 
+/// @brief relation operators rule
 bool ruleCompare(Term *t1, Term *operator, Term * t2);
 
+/// @brief ===, !== rule
 bool ruleEquals(Term *t1, Term *operator, Term * t2);
 
+/// @brief main function, created derivation tree of the expression
+/// @param exp expression to analyze
+/// @param resultType pointer to integer to save type of root of the tree
+/// @return true on success, false on error
 bool bottomUp(Expression *exp, int *resultType);
 
 #endif
